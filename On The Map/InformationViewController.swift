205f53ed77b83e6dc29593522c7116445a078dca
Adam Cmiel <adamcmiel@gmail.com>
2015-12-13 00:22:33 -0800

    [PARTIAL] stub IVC

--- On The Map/InformationViewController.swift
+++ On The Map/InformationViewController.swift
@@ -7,29 +7,46 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class InformationViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    enum State {
+        case Prompt
+        case ReadyToSubmit
     }
     
+    var state: State = .Prompt
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        switch state {
+        case .Prompt:
+            // forward gecode string
+            break
+        case .ReadyToSubmit:
+            // submit information
+            break
+        }
+    }
+    
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var promptLabel: UILabel!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any dditional setup after loading the view.
+        
+        let tbc = tabBarController as! TabBarController
+        if tbc.locationAuthorized {
+            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude, longitude: mapView.userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
